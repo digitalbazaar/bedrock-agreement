18f77f83bbcb7cece72e923af5950ff430ff3933
Mark Cherbaka <mcherbaka@digitalbazaar.com>
2016-08-10 16:51:44 -0400

    Add several tests to bedrock-agreements and make small API change.
    - getAccepted API will return unique values only.
    - Adds the following tests to accept API:
      'accepts two identical agreements specified as strings'
      'accepts five agreements w/ same ID in an array'
      'returns error if agreements is not a string or array'
      'returns an error if actor is not a valid identity'
    - Adds the following tests to getAccepted API:
      'returns empty array from no events'
      'returns one agreement from two events'
      'returns an error if actor is not a valid identity'
    - Clean up some of the tests.
    - Make error messages consistent.
    - Make test names more consistent and descriptive.

--- test/mocha/10-api.js
+++ test/mocha/10-api.js
@@ -19,7 +19,7 @@ describe('bedrock-agreement', function() {
       helpers.removeCollection('eventLog', done);
     });
 
-    it('adds an event for one agreement specified as a string', function(done) {
+    it('accepts one agreement specified as a string', function(done) {
       var agreements = uuid();
       var actor = mockData.identities.regularUser.identity;
       async.auto({
@@ -49,7 +49,6 @@ describe('bedrock-agreement', function() {
             should.exist(event.date);
             event.date.should.be.a('string');
             should.exist(event.resource);
-            event.resource.should.be.an('array');
             event.resource.should.have.length(1);
             event.resource[0].should.equal(agreements);
             should.exist(event.actor);
@@ -61,7 +60,8 @@ describe('bedrock-agreement', function() {
         }]
       }, done);
     });
-    it('adds event for four agreements specified as an array', function(done) {
+
+    it('accepts four agreements specified as an array', function(done) {
       var agreements = [];
       for(var i = 0; i < 4; i++) {
         agreements.push(uuid());
@@ -86,6 +86,66 @@ describe('bedrock-agreement', function() {
         }]
       }, done);
     });
+
+    // tests that accept API does not reject duplicates
+    it('accepts two identical agreements specified as strings', function(done) {
+      var agreementsAlpha = uuid();
+      var agreementsBeta = agreementsAlpha;
+      var actor = mockData.identities.regularUser.identity;
+      async.auto({
+        insertAlpha: function(callback) {
+          brAgreement.accept(actor, agreementsAlpha, function(err, result) {
+            should.not.exist(err);
+            callback(err, result);
+          });
+        },
+        insertBeta: function(callback) {
+          brAgreement.accept(actor, agreementsBeta, function(err, result) {
+            should.not.exist(err);
+            callback(err, result);
+          });
+        },
+        testBoth: ['insertAlpha', 'insertBeta', function(callback, results) {
+          database.collections.eventLog.find({
+            'event.type': 'AgreementAccept',
+            typeSpecific: database.hash(actor.id)
+          }).toArray(function(err, result) {
+            should.not.exist(err);
+            result.should.have.length(2);
+            var event0 = result[0].event;
+            var event1 = result[1].event;
+            event0.resource[0].should.equal(agreementsAlpha);
+            event1.resource[0].should.equal(agreementsAlpha);
+            callback();
+          });
+        }]
+      }, done);
+    });
+
+    // tests that accept API does not reject duplicates in an array
+    it('accepts five agreements w/ same ID in an array', function(done) {
+      var agreementsID = uuid();
+      var agreements = Array(5).fill(agreementsID);
+      var actor = mockData.identities.regularUser.identity;
+      async.auto({
+        insert: function(callback) {
+          brAgreement.accept(actor, agreements, callback);
+        },
+        test: ['insert', function(callback, results) {
+          database.collections.eventLog.find({
+            'event.id': results.insert.event.id
+          }).toArray(function(err, result) {
+            should.not.exist(err);
+            var event = result[0].event;
+            should.exist(event.resource);
+            event.resource.should.have.length(5);
+            event.resource.should.deep.equal(agreements);
+            callback();
+          });
+        }]
+      }, done);
+    });
+
     it('returns error if no AGREEMENT_ACCEPT permission', function(done) {
       var agreements = uuid();
       var actor = mockData.identities.noPermission.identity;
@@ -96,13 +156,47 @@ describe('bedrock-agreement', function() {
         done();
       });
     });
-    it('returns an error if agreements is not a string or array');
-    it('returns an error if actor is not a valid identity');
+
+    it('returns error if agreements is not a string or array', function(done) {
+      var agreements = null; // agreement that is neither a string or an array
+      var actor = mockData.identities.regularUser.identity;
+      brAgreement.accept(actor, agreements, function(err) {
+        should.exist(err);
+        err.toString().should.equal(
+          'TypeError: agreements parameter must be a string or an array.');
+        done();
+      });
+    });
+
+    it('returns an error if actor is not a valid identity', function(done) {
+      var agreements = null; // agreement that is neither a string or an array
+      brAgreement.accept(null, agreements, function(err) {
+        should.exist(err);
+        err.toString().should.equal(
+          'TypeError: actor must be an object with an "id" property.');
+        done();
+      });
+    });
+
   }); // end add
 
   describe('getAccepted API', function() {
     beforeEach(function(done) {
       helpers.removeCollection('eventLog', done);
+    });
+
+    it('returns empty array from no events', function(done) {
+      var actor = mockData.identities.regularUser.identity;
+      async.auto({
+        getAccepted: function(callback) {
+          brAgreement.getAccepted(actor, actor.id, callback);
+        },
+        test: ['getAccepted', function(callback, results) {
+          var result = results.getAccepted;
+          result.should.have.length(0);
+          callback();
+        }]
+      }, done);
     });
 
     it('returns one agreement from one event', function(done) {
@@ -117,13 +211,13 @@ describe('bedrock-agreement', function() {
         }],
         test: ['getAccepted', function(callback, results) {
           var result = results.getAccepted;
-          result.should.be.an('array');
           result.should.have.length(1);
           result.should.include(agreements);
           callback();
         }]
       }, done);
     });
+
     it('returns four agreements from one event', function(done) {
       var agreements = [];
       for(var i = 0; i < 4; i++) {
@@ -139,13 +233,13 @@ describe('bedrock-agreement', function() {
         }],
         test: ['getAccepted', function(callback, results) {
           var result = results.getAccepted;
-          result.should.be.an('array');
           result.should.have.length(4);
           result.should.deep.equal(agreements);
           callback();
         }]
       }, done);
     });
+
     it('returns two agreements from two events', function(done) {
       var agreementsAlpha = uuid();
       var agreementsBeta = uuid();
@@ -162,13 +256,13 @@ describe('bedrock-agreement', function() {
         }],
         test: ['getAccepted', function(callback, results) {
           var result = results.getAccepted;
-          result.should.be.an('array');
           result.should.have.length(2);
-          result.should.deep.equal([agreementsAlpha, agreementsBeta]);
+          result.should.have.same.members([agreementsAlpha, agreementsBeta]);
           callback();
         }]
       }, done);
     });
+
     it('returns five agreements from two events', function(done) {
       var agreementsAlpha = uuid();
       var agreementsBeta = [];
@@ -190,15 +284,37 @@ describe('bedrock-agreement', function() {
         }],
         test: ['getAccepted', function(callback, results) {
           var result = results.getAccepted;
-          result.should.be.an('array');
           result.should.have.length(5);
-          result.should.deep.equal(allAgreements);
+          result.should.have.same.members(allAgreements);
           callback();
         }]
       }, done);
     });
+
+    it('returns one agreement from two events', function(done) {
+      var agreementsAlpha = uuid();
+      var agreementsBeta = Array(4).fill(agreementsAlpha);
+      var actor = mockData.identities.regularUser.identity;
+      async.auto({
+        insertAlpha: function(callback) {
+          brAgreement.accept(actor, agreementsAlpha, callback);
+        },
+        insertBeta: ['insertAlpha', function(callback) {
+          brAgreement.accept(actor, agreementsBeta, callback);
+        }],
+        getAccepted: ['insertBeta', function(callback) {
+          brAgreement.getAccepted(actor, actor.id, callback);
+        }],
+        test: ['getAccepted', function(callback, results) {
+          var result = results.getAccepted;
+          result.should.have.length(1);
+          result.should.have.same.members([agreementsAlpha]);
+          callback();
+        }]
+      }, done);
+    });
+
     it('returns error if no AGREEMENT_ACCESS permission', function(done) {
-      var agreements = uuid();
       var actor = mockData.identities.noPermission.identity;
       brAgreement.getAccepted(actor, actor.id, function(err) {
         should.exist(err);
@@ -207,5 +323,17 @@ describe('bedrock-agreement', function() {
         done();
       });
     });
+
+    it('returns an error if actor is not a valid identity', function(done) {
+      var actor = null;
+      var id = mockData.identities.regularUser.identity.id;
+      brAgreement.getAccepted(actor, id, function(err) {
+        should.exist(err);
+        err.toString().should.equal(
+          'TypeError: actor must be an object with an "id" property.');
+        done();
+      });
+    });
+
   }); // end get
 });
